235a7db293efb25d1e7fd367ac17162568a5b51c
DanrleyBrasil <danrleybrasil.s@gmail.com>
2025-10-28 20:49:46 -0300

    Initial commit: Eureka Server setup

---
+++ src/main/java/com/danlearnings/ecommerce/serviceregistry/EcommerceServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.danlearnings.ecommerce.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceServiceRegistryApplication.class, args);
+    }
+
+}
